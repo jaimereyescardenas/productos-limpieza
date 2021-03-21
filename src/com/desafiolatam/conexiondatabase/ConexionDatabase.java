844b99dad84b3eefdc7f72d3f35c0f579cf7497c
Jaime Reyes Cárdenas <jaimefabianreyescardenas@gmail.com>
2021-03-21 18:23:23 -0300

    Configuración de JSTL y Driver de conexión a base de datos

--- src/com/desafiolatam/conexiondatabase/ConexionDatabase.java
+++ src/com/desafiolatam/conexiondatabase/ConexionDatabase.java
@@ -1,5 +1,28 @@
 package com.desafiolatam.conexiondatabase;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionDatabase {
+	
+	private static final String DRIVER = "oracle.jdbc.oracleDriver";
+	private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	private static final String USERNAME = "desafiolatam";
+	private static final String PASSWORD = "desafiolatam";
+	
+	private Connection conexion = null;
+	
+	public Connection crearConexion() {
+		try {
+			Class.forName(DRIVER);
+			conexion = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return conexion;
+	}
 
 }
